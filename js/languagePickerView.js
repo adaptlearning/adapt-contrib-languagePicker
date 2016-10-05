6f208639e3795831c12d4f9f72ef85b75c403f91
moloko <mleathes+github@gmail.com>
2016-10-05 15:55:33 +0100

    switch to 4-space indentation for consistency with the reset of the Adapt code

--- js/languagePickerView.js
+++ js/languagePickerView.js
@@ -1,61 +1,61 @@
 define([
-  'core/js/adapt',
-  './accessibilityView'
+    'core/js/adapt',
+    './accessibilityView'
 ], function(Adapt, accessibilityView) {
-  
-  var LanguagePickerView = Backbone.View.extend({
     
-    events: {
-      'click .languagepicker-languages button': 'onLanguageClick'
-    },
-    
-    className: 'languagepicker',
-    
-    initialize: function () {
-      this.initializeAccessibility();
+    var LanguagePickerView = Backbone.View.extend({
+        
+        events: {
+            'click .languagepicker-languages button': 'onLanguageClick'
+        },
+        
+        className: 'languagepicker',
+        
+        initialize: function () {
+            this.initializeAccessibility();
 
-      this.listenTo(Adapt, 'remove', this.remove);
-      this.render();
-    },
-    
-    render: function () {
-      var data = this.model.toJSON();
-      var template = Handlebars.templates[this.constructor.template];
-      this.$el.html(template(data));
-      
-      _.defer(_.bind(function () {
-        this.postRender();
-      }, this));
-    },
-    
-    postRender: function () {
-      $('.loading').hide();
-    },
-    
-    onLanguageClick: function (event) {
-      this.destroyAccessibility();
-      this.model.setLanguage($(event.target).val());
-    },
+            this.listenTo(Adapt, 'remove', this.remove);
+            this.render();
+        },
+        
+        render: function () {
+            var data = this.model.toJSON();
+            var template = Handlebars.templates[this.constructor.template];
+            this.$el.html(template(data));
+            
+            _.defer(_.bind(function () {
+                this.postRender();
+            }, this));
+        },
+        
+        postRender: function () {
+            $('.loading').hide();
+        },
+        
+        onLanguageClick: function (event) {
+            this.destroyAccessibility();
+            this.model.setLanguage($(event.target).val());
+        },
 
 
-    initializeAccessibility: function() {
-      $("html").addClass("in-languagepicker");
+        initializeAccessibility: function() {
+            $("html").addClass("in-languagepicker");
 
-      this.accessibilityView = new accessibilityView({
-        model:this.model
-      });
-    },
+            this.accessibilityView = new accessibilityView({
+                model:this.model
+            });
+        },
 
-    destroyAccessibility: function() {
-      $("html").removeClass("in-languagepicker");
+        destroyAccessibility: function() {
+            $("html").removeClass("in-languagepicker");
 
-      this.accessibilityView.remove();
-    }
-    
-  }, {
-    template: 'languagePickerView'
-  });
+            this.accessibilityView.remove();
+        }
+        
+    }, {
+        template: 'languagePickerView'
+    });
 
-  return LanguagePickerView;
+    return LanguagePickerView;
 
 });
