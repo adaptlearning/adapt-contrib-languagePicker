e241db70887c3d440054c5eeb4b259784a06dc4b
Chris Steele <chris.steele@kineo.com>
2017-10-05 12:49:39 +0700

    Move a11y toggle button and instruction to nav bar

--- js/languagePickerNavigationView.js
+++ js/languagePickerNavigationView.js
@@ -1,4 +1,129 @@
 define([
+    'core/js/adapt',
+    './accessibilityView'
+], function(Adapt, AccessibilityView) {
+
+    var NavigationView = Backbone.View.extend({
+
+        className: "navigation",
+
+        initialize: function() {
+            /*this.listenToOnce(Adapt, 'courseModel:dataLoading', this.remove);*/
+            this.listenToOnce(Adapt, 'accessibility:toggle', this.onA11yToggle);
+            /*this.listenTo(Adapt, 'router:menu router:page', this.hideNavigationButton);*/
+            this.template = "languagePickerNavigation";
+            this.setupHelpers();
+            this.preRender();
+        },
+
+        events: {
+            'click [data-event]':'triggerEvent'
+        },
+
+        preRender: function() {
+            Adapt.trigger('navigationView:preRender', this);
+            this.render();
+        },
+
+        render: function() {
+            var template = Handlebars.templates[this.template];
+            this.$el.html(template(
+                {
+                    _config: this.model.get("_accessibility"),
+                    _accessibility: Adapt.config.get("_accessibility")
+                }
+            )).insertBefore('#wrapper');
+
+            _.defer(_.bind(function() {
+                Adapt.trigger('navigationView:postRender', this);
+            }, this));
+
+            this.setupA11yButton();
+
+            if (Adapt.accessibility.isActive()) {
+                this.setupUsageInstructions();
+            }
+
+            return this;
+        },
+
+        setupHelpers: function() {
+            var config = Adapt.config.get("_accessibility");
+
+            Handlebars.registerHelper('a11y_text', function(text) {
+                //ALLOW ENABLE/DISABLE OF a11y_text HELPER
+                if (config && config._isTextProcessorEnabled === false) {
+                    return text;
+                } else {
+                    return $.a11y_text(text);
+                }
+            });
+
+            Handlebars.registerHelper('a11y_aria_label', function(text) {
+                return '<div class="aria-label prevent-default" tabindex="0" role="region">'+text+'</div>';
+            });
+        },
+
+        triggerEvent: function(event) {
+            event.preventDefault();
+            var currentEvent = $(event.currentTarget).attr('data-event');
+            Adapt.trigger('navigation:' + currentEvent);
+        },
+
+        /*hideNavigationButton: function(model) {
+            if (model.get('_type') === "course") {
+                $('.navigation-back-button, .navigation-home-button').addClass('display-none');
+            } else {
+                this.showNavigationButton();
+            }
+        },
+
+        showNavigationButton: function() {
+            $('.navigation-back-button, .navigation-home-button').removeClass('display-none');
+        },*/
+
+        setupA11yButton: function() {
+            new AccessibilityView({model:this.model});
+        },
+
+        setupUsageInstructions: function() {
+            var config = this.model.get("_accessibility");
+
+            if (!config || !config._accessibilityInstructions) {
+                this.$('#accessibility-instructions').remove();
+                return;
+            }
+
+            var instructionsList = config._accessibilityInstructions;
+
+            var usageInstructions;
+            if (instructionsList[Adapt.device.browser]) {
+                usageInstructions = instructionsList[Adapt.device.browser];
+            } else if (Modernizr.touch) {
+                usageInstructions = instructionsList.touch || "";
+            } else {
+                usageInstructions = instructionsList.notouch || "";
+            }
+
+           this.$('#accessibility-instructions').html( usageInstructions );
+        },
+
+        onA11yToggle:function() {
+            // listen once because if a11y active on launch instructions will already be setup
+            
+            if (Adapt.accessibility.isActive()) {
+                this.setupUsageInstructions();
+            }
+        }
+
+    });
+
+    return NavigationView;
+
+});
+
+
+/*define([
     'core/js/adapt'
 ], function(Adapt) {
 
@@ -11,6 +136,8 @@ define([
         },
 
         initialize: function() {
+            this.setupHelpers();
+
             this.setupUsageInstructions();
 
             if(Adapt.offlineStorage.ready) {
@@ -41,7 +168,7 @@ define([
 
                 var toggleText = isActive ? offLabel : onLabel;
 
-                this.$el.attr('aria-label', $.a11y_normalize(toggleText));
+                this.$el.html(toggleText).attr('aria-label', $.a11y_normalize(toggleText));
 
                 if (isActive) {
                     $("html").addClass('accessibility');
@@ -68,6 +195,19 @@ define([
             this.render();
 
             this.trigger('accessibility:toggle');
+        },
+
+        setupHelpers: function() {
+            var config = Adapt.config.get("_accessibility");
+
+            Handlebars.registerHelper('a11y_text', function(text) {
+                //ALLOW ENABLE/DISABLE OF a11y_text HELPER
+                if (config && config._isTextProcessorEnabled === false) {
+                    return text;
+                } else {
+                    return $.a11y_text(text);
+                }
+            });
         },
 
         configureAccessibility: function() {
@@ -136,4 +276,4 @@ define([
 
     return AccessibilityView;
 
-});
+});*/
