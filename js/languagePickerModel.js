69b53e5f7f2cf72245526d69cb562f3548b9addd
Nayan Khedkar <nayan.khedkar@exultcorp.com>
2018-09-05 16:59:19 +0530

    added code to restore state on language change.

--- js/languagePickerModel.js
+++ js/languagePickerModel.js
@@ -12,8 +12,18 @@ define([
             "_languages": []
         },
         
+        trackedData: {
+            "components": [],
+            "blocks": []
+        },
+
+        locationId: null,
+        
         initialize: function () {
             this.listenTo(Adapt.config, 'change:_activeLanguage', this.onConfigChange);
+            if (this.get("_restoreStateOnLangaugeChange")) {
+                this.listenTo(Adapt, 'app:dataLoaded', this.onDataLoaded);
+            }
         },
 
         getLanguageDetails: function (language) {
@@ -46,6 +56,79 @@ define([
             }
 
             this.set('_languages', languages);
+        },
+            
+        onDataLoaded: function() {
+            _.defer(function() {
+                this.locationId = Adapt.offlineStorage.get('location') || null;
+                this.restoreState();
+            }.bind(this));
+
+        },
+        
+        restoreLocation: function() {
+            _.defer(function() {
+                if (this.locationId !== "undefined" && this.locationId) {
+                    Adapt.navigateToElement('.' + this.locationId);
+                }
+            }.bind(this));
+        },
+
+        /**
+         * Restore course progress on language change.
+         */
+        restoreState: function() {
+
+            if (this.isTrackedDataEmpty()) return;
+
+            if (this.trackedData.components) {
+                _.each(this.trackedData.components, this.setTrackableState);
+            }
+
+            if (this.trackedData.blocks) {
+                _.each(this.trackedData.blocks, this.setTrackableState);
+            }
+        },
+
+        isTrackedDataEmpty: function() {
+            return _.isEqual(this.trackedData, {
+                "components": [],
+                "blocks": []
+            });
+        },
+
+        getTrackableState: function() {
+            var components = _.map(Adapt.components.models, function(model) {
+                if (model.get('_isComplete')) {
+                    return model.getTrackableState();
+                }
+            });
+            var blocks = _.map(Adapt.blocks.models, function(model) {
+                if (model.get('_isComplete')) {
+                    return model.getTrackableState();
+                }
+            });
+            return {
+                "components": _.compact(components),
+                "blocks": _.compact(blocks)
+            };
+        },
+
+        setTrackedData: function() {
+            if (this.get("_restoreStateOnLangaugeChange")) {
+                this.listenToOnce(Adapt, "menuView:ready", this.restoreLocation);
+                this.trackedData = this.getTrackableState();
+            }
+        },
+
+        setTrackableState: function(stateObject) {
+            var restoreModel = Adapt.findById(stateObject._id);
+
+            if (restoreModel) {
+                restoreModel.setTrackableState(stateObject);
+            } else {
+                Adapt.log.warn('LanguagePicker unable to restore state for: ' + stateObject._id);
+            }
         }
         
     });
